f206966c7aa043c5375ac733eee01216f9316b65
byro18 <byx7214@pm.me>
2024-07-30 23:04:02 -0400

    layout main elements (wip)

---
+++ dummydata.js
@@ -0,0 +1,6 @@
+export const data = [
+	{ title: 'Where is AI going', date: '02/28/21', blurb: 'Are we doomed?' },
+	{ title: 'Where is AI going', date: '02/28/21', blurb: 'Are we doomed?' },
+	{ title: 'Where is AI going', date: '02/28/21', blurb: 'Are we doomed?' },
+	{ title: 'Where is AI going', date: '02/28/21', blurb: 'Are we doomed?' },
+];
