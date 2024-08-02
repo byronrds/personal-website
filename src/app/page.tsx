db73afd325c5e668e5337c415e8b869938bc58a4
byro18 <byx7214@pm.me>
2024-08-01 23:04:29 -0400

    Add red sidebar listing 'about' and 'messy' sections
    - Delete phony blue 'messy' button
    - Display only blog posts on mounting page. People can go to 'about' page if they want to learn more.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,40 +13,39 @@ export default function Home() {
 		<main>
 			<NextUIProvider>
 				<div>
-					<p className='text-2xl mb-8'>bzrblog</p>
-					<Button className='absolute top-8 right-8' color='primary' onClick={() => handleNavigation()}>
-						messy
-					</Button>
-					<div className='w-2/3 m-auto'>
-						<p className='mb-8'>
-							This is my blog. You can think of it as having two sections. formal and informal. smart and
-							messy. Below you will see some of my more refined thoughts. Click the "messy" button to view
-							some of my thoughts. They are kinda scattered and as you might think, a bit messy, but
-							nonetheless things I think about and would like to talk about. Please make comments on my
-							post and feel free to email me at <a href='mailto:bxr@byronxr.com'>bxr@byronxr.com</a>
-						</p>
+					<div className='flex flex-row'>
+						<div className='fixed top-0 left-0 w-36 h-full bg-red-500'></div>
+						<div className='relative mt-8 z-1 w-20'>
+							<p className='text-2xl mb-4'>bzrblog</p>
+							<ul>
+								<li className='text-sm'>About</li>
+								<li className='text-sm'>Messy</li>
+							</ul>
+						</div>
 
-						<div className='w-3/4'>
-							{data.map((item, index) => {
-								return (
-									<div className='mb-8'>
-										<div
-											key={index}
-											className='grid grid-cols-8 grid-flow-row group hover:cursor-pointer'>
-											<p className='opacity-25 col-start-1 col-end-2 group-hover:opacity-50'>
-												{item.date}
-											</p>
-											<p className='col-start-2 col-end-9 opacity-60 group-hover:opacity-100'>
-												{item.title}
-											</p>
+						<div className='w-2/3 m-auto'>
+							<div className='mt-10 w-3/4'>
+								{data.map((item, index) => {
+									return (
+										<div className='mb-8'>
+											<div
+												key={index}
+												className='grid grid-cols-8 grid-flow-row group hover:cursor-pointer'>
+												<p className='opacity-25 col-start-1 col-end-2 group-hover:opacity-50'>
+													{item.date}
+												</p>
+												<p className='col-start-2 col-end-9 opacity-60 group-hover:opacity-100'>
+													{item.title}
+												</p>
 
-											<p className='col-start-2 col-end-9 opacity-25 group-hover:opacity-50'>
-												{item.blurb}
-											</p>
+												<p className='col-start-2 col-end-9 opacity-25 group-hover:opacity-50'>
+													{item.blurb}
+												</p>
+											</div>
 										</div>
-									</div>
-								);
-							})}
+									);
+								})}
+							</div>
 						</div>
 					</div>
 				</div>
