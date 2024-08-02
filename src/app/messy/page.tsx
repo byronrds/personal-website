1288d946ee4fb9118c3617c505aa20acf4e32322
byronrds <byronrdsss761@gmail.com>
2024-08-01 22:15:35 -0400

    Improve blog layout with grid design and hover effects
    - Add CSS hover effects to increase opacity on hover
    - Integrate navigation button to 'messy' section

---
+++ src/app/messy/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+import { useState, useEffect } from 'react';
+
+import Link from 'next/link';
+
+// this will get the id of the one that is selected from the list
+//id: 0, 1, 2, 3, in dynamodb
+export default function page() {
+	return (
+		<>
+			<h1>Hello there. </h1>
+		</>
+	);
+}
