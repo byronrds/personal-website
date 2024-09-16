08adc1a8130ddf6512b574b02dc957a808ded09b
byronrds <byronrdsss761@gmail.com>
2024-09-15 21:34:33 -0400

    add loading component

---
+++ src/components/LoadingDots.tsx
@@ -0,0 +1,23 @@
+'use client';
+import React, { useState, useEffect } from 'react';
+import { FaCircle } from 'react-icons/fa';
+
+export const LoadingDots = () => {
+	const [dotCount, setDotCount] = useState(0);
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setDotCount((prevCount) => (prevCount + 1) % 4); // cycle 0 to 3
+		}, 400);
+
+		return () => clearInterval(interval); // cleanup on unmount
+	}, []);
+
+	return (
+		<div className='flex'>
+			{[...Array(dotCount)].map((_, index) => (
+				<FaCircle key={index} className='text-sm mx-1 opacity-40' />
+			))}
+		</div>
+	);
+};
