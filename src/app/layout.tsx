5c043e648efb57e79e5524e22aeed99b356f7635
byronrds <byronrdsss761@gmail.com>
2025-04-29 16:32:22 -0400

    rename titile to full name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,37 +1,37 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import './globals.css';
-import { Providers } from './providers';
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { Providers } from "./providers";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: 'byronxr',
-	description: '',
-	icons: {
-		icon: [
-			{
-				media: '(prefers-color-scheme: light)',
-				url: '/images/red.png',
-			},
-			{
-				media: '(prefers-color-scheme: dark)',
-				url: '/images/red.png',
-			},
-		],
-	},
+  title: "Byron Richards",
+  description: "",
+  icons: {
+    icon: [
+      {
+        media: "(prefers-color-scheme: light)",
+        url: "/images/red.png",
+      },
+      {
+        media: "(prefers-color-scheme: dark)",
+        url: "/images/red.png",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang='en' className={inter.className} suppressHydrationWarning>
-			<body>
-				<Providers>{children}</Providers>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en" className={inter.className} suppressHydrationWarning>
+      <body>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
 }
